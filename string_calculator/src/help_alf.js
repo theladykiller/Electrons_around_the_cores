658b431eadf90196403332890745e805307792d1
theladykiller <niki.plachetzky@gmail.com>
2024-05-27 16:46:06 +0200

    Jo: code for todo 3. wrote

--- string_calculator/src/help_alf.js
+++ string_calculator/src/help_alf.js
@@ -1,7 +1,15 @@
 function electrons_function() {}
 electrons_function.prototype.play_game = function(array) {
 	if(array){
-		return 2;
+		let result = 0;
+		array.forEach(element => {
+			if(element === 3){
+				result += 2;
+			}else if(element === 5){
+				result += 4;
+			}
+		});
+		return result;
 	}
 	return "No dice thrown";
 };
