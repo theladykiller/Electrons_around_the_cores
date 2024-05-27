206d824ddaee7bc80f05024e29de3dacb9b0f6b3
theladykiller <niki.plachetzky@gmail.com>
2024-05-27 16:43:39 +0200

    Niki: code for todo 3. wrote

--- string_calculator/spec/help_alf_spec.js
+++ string_calculator/spec/help_alf_spec.js
@@ -3,12 +3,20 @@ describe('String Calculator', function() {
 		electrons = new electrons_function()
 	});
 
+	//todo 1
 	it('should return "No dice thrown" if no variable was passed', function() {
 		expect(electrons.play_game()).toEqual("No dice thrown");
 	});
 
-	it('should return "2" if array is: [1,2,3,4] ', function() {
+	//todo 2
+	it('should return "2" if array is: [1,2,3,4]', function() {
 		let array = [1,2,3,4];
 		expect(electrons.play_game(array)).toEqual(2);
+	});
+
+	//todo 3
+	it('should return "6" if array is: [1,2,3,4,5,6]', function() {
+		let array = [1,2,3,4,5,6];
+		expect(electrons.play_game(array)).toEqual(6);
 	});
 });
