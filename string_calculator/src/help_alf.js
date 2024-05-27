d29732d930b731c1211c0f187c38a5f281d27134
theladykiller <niki.plachetzky@gmail.com>
2024-05-27 16:54:21 +0200

    Niki: code for todo 4

--- string_calculator/src/help_alf.js
+++ string_calculator/src/help_alf.js
@@ -1,11 +1,15 @@
 function electrons_function() {}
 electrons_function.prototype.play_game = function(array) {
 	if(array){
+		if(array.length < 4){
+			return "Not enough dice thrown"
+		}
+
 		let result = 0;
-		array.forEach(element => {
-			if(element === 3){
+		array.forEach(element => { 
+			if(element === 3){	//add 2 for a 3
 				result += 2;
-			}else if(element === 5){
+			}else if(element === 5){ //add 4 for a 5
 				result += 4;
 			}
 		});
